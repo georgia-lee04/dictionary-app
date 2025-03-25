4dd067d2bb3795b25a7607c9064511bdd3a80013
georgia-lee04 <xgeorgialee@gmail.com>
2025-03-25 18:44:01 +0000

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was coded by Georgia Lee and is open-sourced on GitHub
+        </footer>
+      </div>
     </div>
   );
 }
