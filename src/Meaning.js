c819f7f704f82ad5f22ba05429ead15126c097f9
georgia-lee04 <xgeorgialee@gmail.com>
2025-03-28 10:13:57 +0000

    Created Synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,9 +8,13 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <div>
         <p>
+          <strong>Definition: </strong>
           {props.meaning.definition}
           <br />
+          <strong>Example: </strong>
           <em> {props.meaning.example} </em>
+          <br />
+          <Synonyms synonyms={props.meaning.synonyms} />
         </p>
       </div>
     </div>
