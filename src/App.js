e7d7d2d00cd841db3cec041ac608e814d08cab19
georgia-lee04 <xgeorgialee@gmail.com>
2025-03-30 16:50:14 +0100

    Updated background color

--- src/App.js
+++ src/App.js
@@ -10,7 +10,22 @@ export default function App() {
           <Dictionary defaultKeyword="sunset" />
         </main>
         <footer className="App-footer">
-          This project was coded by Georgia Lee and is open-sourced on GitHub
+          This project was coded by{" "}
+          <a
+            href="https://portfolio-georgia-lee.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Georgia Lee
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/georgia-lee04/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
         </footer>
       </div>
     </div>
